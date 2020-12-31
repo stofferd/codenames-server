e111a415506a1c12ebf089ae2cf5c195d9e1df74
Chris <chrispdunne@gmail.com>
2020-12-31 08:53:33 +0000

    trying out rooms

--- index.js
+++ index.js
@@ -11,9 +11,29 @@ app.get("/", (req, res, next) => {
   res.send("<h1>Hello world</h1>");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  console.log(socket.rooms);
+// io.on("connection", (socket) => {
+
+//     socket.join('some room');
+
+//   console.log("a user connected");
+//   console.log(socket.rooms);
+
+// });
+
+io.sockets.on("connection", function (socket) {
+  socket.on("join", function (room) {
+    socket.join(room);
+    // console.log(socket);
+  });
+  socket.on("testes", (arg) => {
+    console.log(arg);
+    socket.to("room1").emit("test2", "HEYHEYHEY");
+
+    const clientsList = io.sockets.adapter.rooms["room1"];
+    // var numClients = clientsList.length;
+
+    socket.to("room1").emit("clients", clientsList);
+  });
 });
 
 http.listen(3000, () => {
